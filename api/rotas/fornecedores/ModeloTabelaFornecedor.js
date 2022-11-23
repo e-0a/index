9b70e69b9fdc6ebaddeb257620de3d8be3b90bc6
e-0a <118397019+e-0a@users.noreply.github.com>
2022-11-22 22:42:52 -0300

    modelo-tabela- fornecedor

---
+++ api/rotas/fornecedores/ModeloTabelaFornecedor.js
@@ -0,0 +1,30 @@
+// 22/11/22 > Esse arquivo será revisado até 02/12 
+
+const Sequelize = require('sequelize')
+const instancia = require('../../banco-de-dados')
+
+const colunas = {
+    empresa: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    categoria: {
+        type: Sequelize.ENUM('ração', 'brinquedos'),
+        allowNull: false
+    }
+}
+
+const opcoes = {
+    freezeTableName: true,
+    tableName: 'fornecedores',
+    timestamps: true,
+    createdAt: 'dataCriacao',
+    updatedAt: 'dataAtualizacao',
+    version: 'versao'
+}
+
+module.exports = instancia.define('fornecedor', colunas, opcoes)
